8df132aa5a64432618dac9fa2f7d71f59c502311
互联网-lg <liugang2@bsoft.com.cn>
2021-07-31 09:15:49 +0800

    ADD:添加基座模块

---
+++ main/vite.config.ts
@@ -0,0 +1,42 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+import { resolve } from "path";
+// https://vitejs.dev/config/
+export default defineConfig({
+  define: {
+    "process.env": {},
+  },
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"), // 设置 `@` 指向 `src` 目录
+    },
+  },
+  // 全局css样式变量
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "./src/styles/_variable.scss";`,
+      },
+    },
+  },
+  base: "./", // 设置打包路径
+  build: {
+    outDir: "../../qiankun/main",
+  },
+  // 本地运行配置，及反向代理配置
+  server: {
+    port: 1000,
+    cors: true, // 默认启用并允许任何源
+    open: true, // 在服务器启动时自动在浏览器中打开应用程序
+    // 反向代理配置，注意rewrite写法，开始没看文档在这里踩了坑
+    proxy: {
+      "/infra": {
+        target: "http://rap2api.taobao.org/app/mock/283135/", // 代理接口
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/infra/, ""),
+      },
+    },
+  },
+});
