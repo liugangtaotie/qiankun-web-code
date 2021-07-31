db5a0e62ac663b2da143143607925328b03e5086
互联网-lg <liugang2@bsoft.com.cn>
2021-07-31 12:59:18 +0800

    MOD:成功将main-vue2加入qiankun

--- main-vue2/src/views/Home.vue
+++ main-vue2/src/views/Home.vue
@@ -1,18 +1,99 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+
+    <div class="system">
+      <div v-for="(item, index) in modeuleList" :key="index">
+        <div
+          @click="$router.push(item.path)"
+          class="module-system"
+          :class="item.systemId == activeModule ? 'active-module' : ''"
+          @mouseover="activeModule = item.systemId"
+          @mouseleave="activeModule = ''"
+        >
+          <img
+            :src="item.systemId == activeModule ? item.selected : item.normal"
+          />
+          <div class="module-title">{{ item.title }}</div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import { registerMicroApps, start } from "qiankun";
 
 @Component({
-  components: {
-    HelloWorld,
-  },
+  components: {},
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  activeModule = ""; // 系统模块
+
+  modeuleList = [
+    {
+      developmentEntry: "//localhost:4000/",
+      enabled: true,
+      name: "webpack-app",
+      normal: "images/module/dvs-farm-normal.png",
+      path: "/webpack-app/about",
+      productionEntry: "/child/webpack-app/",
+      selected: "images/module/dvs-farm-selected.png",
+      systemId: "webpack-app",
+      title: "webpack-app",
+    },
+    {
+      developmentEntry: "//localhost:5000/",
+      enabled: true,
+      name: "map-app",
+      normal: "images/module/dvs-village-normal.png",
+      path: "/map-app/about",
+      productionEntry: "/child/map-app/",
+      selected: "images/module/dvs-village-selected.png",
+      systemId: "map-app",
+      title: "map-app",
+    },
+  ];
+
+  getActiveRule = (hash: any) => (location: any) =>
+    location.hash.startsWith(hash);
+
+  mounted() {
+    const array = [
+      {
+        container: "#mainwrapper",
+        entry: "//localhost:4000/",
+        name: "webpack-app",
+        activeRule: this.getActiveRule("#/webpack-app"),
+      },
+      {
+        container: "#mainwrapper",
+        entry: "//localhost:5000/",
+        name: "map-app",
+        activeRule: this.getActiveRule("#/map-app"),
+      },
+    ];
+
+    console.log("start loading");
+    // TODO  在主应用中注册微应用
+    registerMicroApps([...array]);
+
+    // TODO 启动微应用
+    start({
+      prefetch: "all",
+      sandbox: {
+        // strictStyleIsolation: true,
+        experimentalStyleIsolation: true,
+      },
+    });
+  }
+
+  // 跳转sub-first
+  gotoSubOne() {
+    // this.$route.push('/webpack-app/about')
+  }
+
+  gotoSubReact() {}
+}
 </script>
